0dead4db6368582666954263886ba7cdfab9d9b4
Srivas Chennu <srivas@gmail.com>
2013-06-06 23:00:06 +0000

    Checked in Fri Jun  7 00:00:06 BST 2013
    git-svn-id: file:///Users/chennu/Dropbox/Sources/Integration@15 b2874634-da1c-4ccf-8a7e-c0cba462ac32

--- paradigm/Integration.m
+++ paradigm/Integration.m
@@ -54,23 +54,23 @@ if ~exist('DEBUG','var') || DEBUG == 0
         if ~isempty(audiodevices)
             %DMX audio
             outdevice = strcmp('DMX 6Fire USB ASIO Driver',{audiodevices.DeviceName});
-            hd.outdevice = 2;
+            hd.outdevice = 'dmx';
         else
             %Windows default audio
             audiodevices = PsychPortAudio('GetDevices',2);
             outdevice = strcmp('Microsoft Sound Mapper - Output',{audiodevices.DeviceName});
-            hd.outdevice = 3;
+            hd.outdevice = 'base';
         end
     elseif ismac
         audiodevices = PsychPortAudio('GetDevices');
         %DMX audio
         outdevice = strcmp('TerraTec DMX 6Fire USB',{audiodevices.DeviceName});
-        hd.outdevice = 2;
+        hd.outdevice = 'dmx';
         if sum(outdevice) ~= 1
             %Mac default audio
             audiodevices = PsychPortAudio('GetDevices');
             outdevice = strcmp('Built-in Output',{audiodevices.DeviceName});
-            hd.outdevice = 1;
+            hd.outdevice = 'base';
         end
     else
         error('Unsupported OS platform!');
