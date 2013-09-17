4d184a0ec43aa73e1da113bc5d2c18ad1dc96290
Srivas Chennu <srivas@gmail.com>
2013-09-17 23:00:13 +0000

    Checked in Wed Sep 18 00:00:12 BST 2013
    git-svn-id: file:///Users/chennu/Dropbox/Sources/Integration@17 b2874634-da1c-4ccf-8a7e-c0cba462ac32

--- plotcomp.m
+++ plotcomp.m
@@ -5,13 +5,14 @@ loadpaths
 filename = [basename '_epochs.set'];
 EEG = pop_loadset('filename', filename, 'filepath', filepath);
 
-data = (EEG.icaweights*EEG.icasphere)*reshape(EEG.data(EEG.icachansind,:,:), length(EEG.icaweights(1,:)), EEG.trials*EEG.pnts);
+data = (EEG.icaweights*EEG.icasphere)*reshape(EEG.data, EEG.nbchan, EEG.trials*EEG.pnts);
 data = reshape( data, size(data,1), EEG.pnts, EEG.trials);
 [spectra,freqs] = spectopo(data,EEG.pnts,EEG.srate,'plot','off');
 
 meanpwr = zeros(size(spectra,1),2);
 meanpwr(:,1) = meanpwr(:,1) + mean(spectra(:,freqs >= freqband(1) & freqs <= freqband(2)),2);
-meanpwr(:,2) = meanpwr(:,2) + mean(spectra(:,freqs >= (freqband(1)-1) & freqs <= (freqband(2)-1)),2);
+meanpwr(:,2) = meanpwr(:,2) + mean(spectra(:,freqs >= (freqband(1)-length(freqband(2)-freqband(1))) ...
+    & freqs <= (freqband(2)-length(freqband(2)-freqband(1)))),2);
 
 
 meanpwr = meanpwr(:,1) - meanpwr(:,2);
