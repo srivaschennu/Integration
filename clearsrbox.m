26bb95d71c987f435d641fed1ca4d26a9a4a60ad
Srivas Chennu <srivas@gmail.com>
2017-09-19 23:45:57 +0100

    Checked in Tue 19 Sep 2017 23:45:57 BST

--- clearsrbox.m
+++
@@ -1,5 +0,0 @@
-function clearsrbox(srboxportobj)
-
-while get(srboxportobj,'BytesAvailable')
-    fread(srboxportobj,get(srboxportobj,'BytesAvailable'));
-end
--- readsrbox.m
+++
@@ -1,8 +0,0 @@
-function value = readsrbox(srboxportobj)
-
-if get(srboxportobj,'BytesAvailable')
-    input = fread(srboxportobj,get(srboxportobj,'BytesAvailable'));
-    value = input(1);
-else
-    value = 0;
-end
